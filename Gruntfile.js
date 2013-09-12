0f661bf3b20d06028638ff6095fcb9e09b54c7d7
Charlie Cochran <charlie.cochran@gmail.com>
2013-09-11 23:10:53 -0400

    Better build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,38 @@ module.exports = function (grunt) {
 			views: {
 				src: ['js/views/*.js'],
 				dest: 'js/views.js'
+			},
+			appjs: {
+				src: [
+					'js/libs/jquery-1.10.2.min.js',
+					'js/libs/handlebars-1.0.0.js',
+					'js/libs/ember-1.0.0.js',
+					'js/libs/prettify.js',
+					'js/app.js',
+					'js/templates.js',
+					'js/views.js',
+					'js/router.js',
+					'js/routes.js',
+					'js/controllers.js'
+				],
+				dest: 'build/sss.js'
+			},
+			appcss: {
+				src: [
+					'css/bootstrap.min.css',
+					'css/prettify.css',
+					'css/sunburst.css',
+					'css/style.css'
+				],
+				dest: 'build/sss.css'
+			}
+		},
+		copy: {
+			main: {
+				expand:true,
+				cwd: 'public',
+				src: ['**'],
+				dest: 'build'
 			}
 		},
 		emberTemplates: {
@@ -31,8 +63,8 @@ module.exports = function (grunt) {
 		},
 		watch: {
 			concat: {
-				files: ['js/controllers/*.js', 'js/routes/*.js', 'js/views/*.js'],
-				tasks: ['concat:controllers', 'concat:routes', 'concat:views']
+				files: ['js/controllers/*.js', 'js/routes/*.js', 'js/views/*.js', 'css/*.css'],
+				tasks: ['concat:controllers', 'concat:routes', 'concat:views', 'concat:appjs', 'concat:appcss']
 			},
 			emberTemplates: {
 				files: 'js/templates/*.hbs',
@@ -42,10 +74,11 @@ module.exports = function (grunt) {
 	});
 	
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-ember-templates');
 	
 	// Default task(s).
-	grunt.registerTask('default', ['emberTemplates']);
+	grunt.registerTask('default', ['emberTemplates', 'concat', 'copy']);
 
 };

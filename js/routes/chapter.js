6d1c088a990f201d9b7393844472d2cdbb6c3165
Charlie Cochran <charlie.cochran@gmail.com>
2013-09-12 21:22:47 -0400

    Scroll to top of page when transitioning chapters

--- js/routes/chapter.js
+++ js/routes/chapter.js
@@ -1,5 +1,8 @@
 App.ChapterRoute = Em.Route.extend({
 	model: function(params) {
 		return App.chapters.findBy('id', params.chapter_id);
+	},
+	afterModel: function() {
+		window.scrollTo(window.scrollX, 0);
 	}
 });
